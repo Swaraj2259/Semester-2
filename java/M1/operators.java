b9c59b2eb294bf626c634e8809fc3d327077ba3f
swaraj wattamwar <swarajmahesh83@gmail.com>
2025-01-06 23:03:38 +0530

    added

---
+++ java/M1/operators.java
@@ -0,0 +1,31 @@
+// demonastrion of operators in Java
+public class operators {
+    public static void main(String[] args) {
+        int a = 1+1; //2
+        int b = a*3; //4
+        int c = b/4; //1
+        int d = c-a; //-1
+        int e = -d; //1
+        System.out.printf("a=%d b=%d c=%d d=%d e=%d", a, b, c, d, e);
+        System.out.println("\nFloating Point Arithmetic");
+        double da = 1+1; //2.0 
+        double db = da*3; //
+        double dc = db /4;
+        double dd = dc - a;
+        double de = -dd;
+        System.out.printf("a=%f, b=%f, c=%f, d=%f , c=%f", da,db,dd,dc,de);
+    }
+}
+        
+
+
+        
+        // // Logical Operators
+        // boolean x = true;
+        // boolean y = false;
+        // System.out.println("Logical AND: " + (x && y));
+        // System.out.println("Logical OR: " + (x || y));
+        // System.out.println("Logical NOT: " + (!x));
+
+        
+    
