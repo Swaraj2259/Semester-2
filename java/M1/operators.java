3e3c6355db7857aa41bc0d89a606b17fbc86c81d
swaraj wattamwar <swarajmahesh83@gmail.com>
2025-01-07 22:46:37 +0530

    added

--- java/M1/operators.java
+++ java/M1/operators.java
@@ -1,21 +1,21 @@
 // demonastrion of operators in Java
 public class operators {
     public static void main(String[] args) {
-        int a = 1+1; //2
-        int b = a*3; //4
-        int c = b/4; //1
-        int d = c-a; //-1
-        int e = -d; //1
-        System.out.printf("a=%d b=%d c=%d d=%d e=%d", a, b, c, d, e);
-        System.out.println("\nFloating Point Arithmetic");
-        double da = 1+1; //2.0 
-        double db = da*3; //
-        double dc = db /4;
-        double dd = dc - a;
-        double de = -dd;
-        System.out.printf("a=%f, b=%f, c=%f, d=%f , c=%f", da,db,dd,dc,de);
-    }
-}
+    //     int a = 1+1; //2
+    //     int b = a*3; //4
+    //     int c = b/4; //1
+    //     int d = c-a; //-1
+    //     int e = -d; //1
+    //     System.out.printf("a=%d b=%d c=%d d=%d e=%d", a, b, c, d, e);
+    //     System.out.println("\nFloating Point Arithmetic");
+    //     double da = 1+1; //2.0 
+    //     double db = da*3; //
+    //     double dc = db /4;
+    //     double dd = dc - a;
+    //     double de = -dd;
+    //     System.out.printf("a=%f, b=%f, c=%f, d=%f , c=%f", da,db,dd,dc,de);
+    // }
+
         
 
 
@@ -27,5 +27,16 @@ public class operators {
         // System.out.println("Logical OR: " + (x || y));
         // System.out.println("Logical NOT: " + (!x));
 
-        
+
+int a = 10, b = 20;
+
+//syntax for the ternary operator
+// condition ? expression1 : expression2;
+
+// Using the ternary operator
+int max = (a > b) ? a : b;
+
+System.out.println("Maximum is: " + max);
+    }
+}
     
