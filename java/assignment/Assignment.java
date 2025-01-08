b536b2dfea152fefce6c65a4ccb5138416e58fe8
swaraj wattamwar <swarajmahesh83@gmail.com>
2025-01-08 23:33:10 +0530

    added

---
+++ java/assignment/Assignment.java
@@ -0,0 +1,79 @@
+//Compound operators
+public class Assignment {
+    public static void main(String[] args) {
+        // Compound operators
+        int a = 50;
+        int b = 5;
+        double c = 40.0;
+        double d = 4.0;
+         int e = 10;
+
+        a += b; 
+        System.out.println("\na after a += b: " + a); 
+
+        a -= b; 
+        System.out.println("a after a -= b: " + a); 
+ 
+        a *= b; 
+        System.out.println("a after a *= b: " + a); 
+
+        c /= d;
+        System.out.println("c after c /= d: " + c); 
+
+        e %= 3; 
+        System.out.println("e after e %= 3: " + e); 
+
+
+
+
+
+
+        // Pre increment and post increment operators
+        // int x = 5;
+        // int y=5;
+
+        // int a = ++x; 
+        // System.out.println("After pre-increment, x: " + x + ",a: " + a);
+
+        // int b = y++; 
+        // System.out.println("After post-increment, y: " + y + ", b: " + b);
+
+        // int c = --x; 
+        // System.out.println("After pre-decrement, x: " + a + ", c: " + c);
+
+        // int d = x--; 
+        // System.out.println("After post-decrement, x: " + a + ", d: " + d);
+
+
+
+
+        //logical operators
+        // boolean a=true;
+        // boolean b=false;
+
+        // System.out.println("a & b: " + (a & b)); 
+
+        // System.out.println("a | b: " + (a | b)); 
+      
+        // System.out.println("a ^ b: " + (a ^ b)); //xor
+
+        // System.out.println("a || b: " + (a || b)); //logical or
+
+        // System.out.println("a && b: " + (a && b)); //Logical AND 
+
+        // System.out.println("a &= b: " + (a &= b)); //
+
+        // System.out.println("a |= b: " + (a |= b)); 
+
+        // System.out.println("a ^= b: " + (a ^= b));
+
+        // System.out.println("!A: " + (!a));  
+
+        // System.out.println("!B: " + (!b)); 
+}
+}
+
+
+
+     
+    
