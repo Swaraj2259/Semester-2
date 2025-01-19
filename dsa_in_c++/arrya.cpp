60439244f15781262904c6896270147247c1f5c0
swaraj wattamwar <swarajmahesh83@gmail.com>
2025-01-19 16:36:25 +0530

    added

--- dsa_in_c++/arrya.cpp
+++ dsa_in_c++/arrya.cpp
@@ -25,8 +25,8 @@
 //     return 0;
 // }
 
-#include <iostream> // Use the correct header file
-using namespace std;
+// #include <iostream> // Use the correct header file
+// using namespace std;
 
 // int main() {
 //     int i;
@@ -56,28 +56,89 @@ using namespace std;
 //     }
 //     cout << endl;
 // }
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int loc = -1, num;
+//     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//     cout << "Enter the number you want to search: ";
+//     cin >> num;
+
+//     for (int i = 0; i < 10; i++) { 
+//         if (arr[i] == num) {
+//             loc = i; // Store the location (index)
+//             break;   // Exit the loop once the number is found
+//         }
+//     }
+
+//     if (loc != -1) {
+//         cout << "Number found at position: " << loc << endl; // Display 1-based position
+//     } else {
+//         cout << "Number not found in the array." << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     int siz = 10;
+//     cout<<sizeof(arr)/sizeof(int)<<endl;
+//     return 0;
+// };
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int size=6;
+//     int mark[size];
+
+//     for(int i=0;i<size;i++){
+//         cin>>mark[i];
+//     }
+//     for(int i=0;i<size;i++){
+//         cout<<mark[i]<<endl;
+//     }
+
+//     return 0;
+// };
+
 #include <iostream>
+// #include <climits> // For INT_MAX and INT_MIN
 using namespace std;
 
 int main() {
-    int loc = -1, num;
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // Predefined array
+    int arr[] = {12, 5, 8, 20, 3};
+    int n = sizeof(arr) / sizeof(arr[0]); // Calculate the size of the array
 
-    cout << "Enter the number you want to search: ";
-    cin >> num;
+    int smallest = INT_MAX; // Initialize to the maximum possible integer
+    int largest = INT_MIN;  // Initialize to the minimum possible integer
+    int smallestIndex = -1, largestIndex = -1;
 
-    for (int i = 0; i < 10; i++) { 
-        if (arr[i] == num) {
-            loc = i; // Store the location (index)
-            break;   // Exit the loop once the number is found
+    // Find smallest and largest numbers and their indices
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i];
+            smallestIndex = i;
+        }
+        if (arr[i] > largest) {
+            largest = arr[i];
+            largestIndex = i;
         }
     }
 
-    if (loc != -1) {
-        cout << "Number found at position: " << loc << endl; // Display 1-based position
-    } else {
-        cout << "Number not found in the array." << endl;
-    }
+    // Output the results
+    cout << "Smallest number: " << smallest << ", Index: " << smallestIndex << endl;
+    cout << "Largest number: " << largest << ", Index: " << largestIndex << endl;
 
     return 0;
 }
