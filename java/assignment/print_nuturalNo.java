adc44e415a6b09246d872db51556dcedd0662c54
swaraj wattamwar <swarajmahesh83@gmail.com>
2025-01-20 22:35:18 +0530

    added

---
+++ java/assignment/print_nuturalNo.java
@@ -0,0 +1,11 @@
+// write a progarm to print all nutural number in reverse
+
+class print_nuturalNo {
+
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+
+        }
+    }
+}
