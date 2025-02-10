1c5a8ec977a4d46ddc4aa58a3adceb5c8c75abd0
Swaraj2259 <swarajmahesh83@gmail.com>
2025-02-10 22:42:50 +0530

    added

---
+++ java_workshop/demo4.java
@@ -0,0 +1,10 @@
+public class demo4{
+    public void area(int a,int b){
+        int a=l*b;
+        System.out.println("Area of rectangle is "+a);
+    }
+    public void area(int a){
+        float a=3.14f*l*l;
+        System.out.println("Area of circle is "+a);
+    }
+}
