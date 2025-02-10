722d680340e08f206818f48880aa0ba8aea6baa8
Swaraj2259 <swarajmahesh83@gmail.com>
2025-02-10 22:40:37 +0530

    added

---
+++ java_workshop/OddEven.java
@@ -0,0 +1,83 @@
+// import java.util.Scanner;
+
+public class OddEven{
+    // public static void main(String[] args) {
+    //     int num ;
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.print("Enter a number: ");
+    //     num = scanner.nextInt();
+    //     if (num % 2 == 0) {
+    //         System.out.println("The number is even");
+    //     } else {
+    //         System.out.println("The number is odd");
+    //     }
+    
+
+    //      System.out.println("entre a character"); 
+    //      char input = 'a';
+    //      switch (input) {
+    //         case 'a':
+    //         System.out.println("You entered a");
+    //         break;
+    //      case 'b':
+    //         System.out.println("You entered b");
+    //         break;
+    //      case 'c':
+    //         System.out.println("You entered c");
+    //         break;
+    //      default:
+    //         System.out.println("You entered something else");
+    //         break;
+  
+    // }
+    // // table of number
+    // for (int i = 2; i < 10; i++) {
+    //     System.err.println(i);
+    // }
+    //square of number tell from 1 to 25
+    // for (int i = 1; i <= 25; i++) {
+    //     System.out.println(i*i);
+    // }
+    //square of number tell from 1 to 25
+    // int i = 1;
+    // while (i <= 25) {
+    //     System.out.println(i*i);
+    //     i++;
+    // }
+    // marks[k]=scanner.nextInt();
+    // //simple example of jagged array
+    // int[][] marks = new int[3][];
+    // marks[0] = new int[3];
+    // marks[1] = new int[2];
+    // marks[2] = new int[4];
+    // for (int i = 0; i < marks.length; i++) {
+    //     for (int j = 0; j < marks[i].length; j++) {
+    //         marks[i][j] = scanner.nextInt();
+    //     }
+    // }
+    // for (int i = 0; i < marks.length; i++) {
+    //     for (int j = 0; j < marks[i].length; j++) {
+    //         System.out.print(marks[i][j] + " ");
+    //     }
+    //     System.out.println();
+    // }
+    //factorial of number
+    // public static int add(int n, int m) {
+    //     return n + m;
+        
+    // }
+    // public static int factorial(int n, int m) {
+    //     if (n == 0) {
+    //         return 1;
+    //     } else {
+    //         return n * factorial(n - 1, m);
+    //     }
+        
+    // }
+    //plindrome example
+    public static void main(String[] args) {
+        int s = add(5, 6);
+        System.out.println(s);
+        display
+    }
+}
