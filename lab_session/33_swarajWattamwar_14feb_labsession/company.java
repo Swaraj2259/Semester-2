e2fbb56972b9f2c50a96a5034a8cb2b9729b6109
Swaraj2259 <swarajmahesh83@gmail.com>
2025-02-16 17:17:48 +0530

    added

---
+++ lab_session/33_swarajWattamwar_14feb_labsession/company.java
@@ -0,0 +1,69 @@
+// package lab_session.14thFeb;
+class employee{
+    int empId;
+    String empName;
+
+    employee(int id, String name) {
+        this.empId = id;
+        this.empName = name;
+    }
+
+    void display() {
+        System.out.println("ID: " + empId + ", Name: " + empName);
+    }
+}
+
+    
+class manger extends employee{
+    String department;
+
+    public manger(int id,String name,String department){
+        super(id,name);
+        this.department = department;
+
+
+    }
+    void display(){
+        System.out.println("ID: " + empId + ", Name: " + empName + ", Department: " + department);
+    }
+
+    
+
+}
+class engineer extends employee{
+    String project;
+
+    public engineer(int id,String name,String project){
+        super(id,name);
+        this.project = project;
+    }
+    void display(){
+        System.out.println("ID: " + empId + ", Name: " + empName + ", Project: " + project);
+    }
+
+}
+class Salesperson extends employee{
+    int sales;
+
+    public Salesperson(int id,String name,int sales){
+        super(id,name);
+        this.sales = sales;
+    }
+    void display(){
+        System.out.println("ID: " + empId + ", Name: " + empName + ", Sales: " + sales);
+    }
+
+}
+public class company {
+    public static void main(String[] args) {
+        manger m1 = new manger(1,"Prasad","IT");
+        m1.display();
+       
+        engineer e1 = new engineer(3,"Rahul","Java");
+        e1.display();
+       
+        Salesperson s1 = new Salesperson(5,"Raj",10000);
+        s1.display();
+       
+    }
+}
