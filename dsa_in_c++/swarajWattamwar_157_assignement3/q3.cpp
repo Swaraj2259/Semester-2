3706500998e4d7517ea466305da358b9da3f35a7
Swaraj2259 <swarajmahesh83@gmail.com>
2025-02-19 23:04:06 +0530

    Added assignment3

---
+++ dsa_in_c++/swarajWattamwar_157_assignement3/q3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//wap to swap two numbers using pointers without using third variable
+int main()
+{
+    int *a=new int;
+    int *b=new int;
+    cout<<"Enter the value of a and b"<<endl;
+    cin>>*a>>*b;
+    cout<<"Before swapping a="<<*a<<" b="<<*b<<endl;
+    *a=*a+*b;
+    *b=*a-*b;
+    *a=*a-*b;
+    cout<<"After swapping a="<<*a<<" b="<<*b<<endl;
+    delete a;
+    delete b;
+    
+}
